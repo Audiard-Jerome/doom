02929b1ac411a9b7ea4619c851d007db5429d8dc
Jérôme <jerome.audiard@gmail.com>
2024-08-18 17:36:54 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+Dos(document.getElementById("dos"), {
+    url: "doom.jsdos",
+    theme : "synthwave",
+    backend : "dosbox",
+    backendLocked : "true",
+    autoStart: "false",
+    kiosk : "true",
+});
